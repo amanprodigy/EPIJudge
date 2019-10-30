b1a0482604d0111c11b9ab6b922af06b2fd4dd54
Aman Srivastava <amanprodigy@gmail.com>
2019-10-30 10:09:36 +0530

    dutch national flag wip

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -1,4 +1,5 @@
 import functools
+import ipdb
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
@@ -8,8 +9,40 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
-    return
+    mid_index = pivot_index
+    cursor = 0
+    ipdb.set_trace()
+    while cursor < len(A):
+        print('Cursor at ', cursor)
+        print('Pivot at ', pivot_index)
+        print(A)
+        if cursor == pivot_index:
+            cursor += 1
+            print('Cursor at pivot. Increasing the cursor')
+            continue
+        if A[cursor] > A[pivot_index] and cursor < pivot_index:
+            A[cursor], A[pivot_index] = A[pivot_index], A[cursor]
+            pivot_index = cursor
+            cursor += 1
+            print('Greater element in lower range.'
+                  ' Swapping and putting pivot back')
+            continue
+        if A[cursor] < A[pivot_index] and cursor > pivot_index:
+            A[cursor], A[pivot_index] = A[pivot_index], A[cursor]
+            pivot_index = cursor
+            cursor += 1
+            print('Smaller element in greater range. Swapping '
+                  'and putting pivot to front')
+            continue
+        if A[cursor] == A[pivot_index] and cursor != mid_index:
+            A[cursor], A[mid_index] = A[mid_index], A[cursor]
+            cursor += 1
+            continue
+        cursor += 1
+        continue
+        # if A[cursor] == A[pivot_index] and cursor == mid_index:
+        #     mid_index += 1
+    return A
 
 
 @enable_executor_hook
@@ -39,7 +72,10 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("dutch_national_flag.py",
-                                       'dutch_national_flag.tsv',
-                                       dutch_flag_partition_wrapper))
+    # exit(
+    #     generic_test.generic_test_main("dutch_national_flag.py",
+    #                                    'dutch_national_flag.tsv',
+    #                                    dutch_flag_partition_wrapper))
+    A = [1, 1, 0, 2]
+    dutch_flag_partition(1, A)
+    print(A)
